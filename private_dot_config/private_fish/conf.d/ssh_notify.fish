6406e2ed32abb57c3b89b71ec6ce6677fc75aa55
柒佰拾零 <yan710010@126.com>
2026-01-04 20:58:25 +0800

    fix: 取消了先前的ssh提示回复
    发现会变成ssh中fish的子进程...不太干净，算了

--- private_dot_config/private_fish/conf.d/ssh_notify.fish
+++ private_dot_config/private_fish/conf.d/ssh_notify.fish
@@ -11,9 +11,9 @@ if status is-interactive && set -q SSH_CONNECTION
     if test $SSH_USER_NAME != Yan -a $SSH_USER_NAME != 27Onion
         # 发送提示语
         if test $SSH_USER_EXISTS = true
-            notify-send --app-name SSH --urgency critical "有人连上来了喵?" "公钥所有者: $SSH_USER_NAME, 于 $(date +"%Y/%m/%d %H:%M:%S")" -A 发现你了哦=回复 &
+            notify-send --app-name SSH --urgency critical "有人连上来了喵?" "公钥所有者: $SSH_USER_NAME, 于 $(date +"%Y/%m/%d %H:%M:%S")"
         else
-            notify-send --app-name SSH --urgency critical "有人连上来了喵?" "公钥: $SSH_USER_HASH, 于 $(date +"%Y/%m/%d %H:%M:%S")" -A "...你谁"=回复 &
+            notify-send --app-name SSH --urgency critical "有人连上来了喵?" "公钥: $SSH_USER_HASH, 于 $(date +"%Y/%m/%d %H:%M:%S")"
         end
     end
     # motd
