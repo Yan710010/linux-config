a2eb3360d4dea59b7fd6d49b9e16bfc6ff688472
柒佰拾零 <yan710010@126.com>
2026-07-30 18:55:23 +0800

    feat(prompt): 修改已完成提示符字符为$
    $方便复制粘贴显示一些，PowerLine字符粘贴出去变成神秘空白了

--- private_dot_config/private_fish/conf.d/starship.fish
+++ private_dot_config/private_fish/conf.d/starship.fish
@@ -7,7 +7,7 @@ function starship_transient_prompt_func
   starship module time
   starship module directory
   echo
-  printf "\x1b[38;2;44;211;211m\x1b[0m "
+  printf "\x1b[38;2;44;211;211m\$\x1b[0m "
 end
 starship init fish | source
 enable_transience
